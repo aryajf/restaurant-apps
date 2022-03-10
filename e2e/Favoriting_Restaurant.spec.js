cfe93c37841013b6abd957389c61370342cbc08d
aryajf <aryajavasfatih888@gmail.com>
2022-03-10 14:55:05 +0700

    jangan bertele tele

--- e2e/Favoriting_Restaurant.spec.js
+++ e2e/Favoriting_Restaurant.spec.js
@@ -1,18 +1,29 @@
-Feature('Favoriting Restaurant');
-
-Before(({ I }) => {
-    I.amOnPage('/#/favorite');
+Feature('favoriting Restaurants');
+Before((I) => {
+  I.amOnPage('/#/favorite');
+});
+Scenario('showing empty favorited restaurants', (I) => {
+  I.seeElement('#query');
+  // I.seeElement('.query'); // membuat test menjadi gagal
+  I.see('Tidak ada film untuk ditampilkan', '.restaurant-item__not__found');
+});
+Scenario('favoriting one restaurant', (I) => {
+  I.see('Tidak ada film untuk ditampilkan', '.restaurant-item__not__found');
+  I.amOnPage('/');
+  // … kita akan mengisi uji coba berikutnya …
 });
 
-Scenario('showing empty favorited restaurants', ({ I }) => {
-    I.seeElement('#query');
-    // I.seeElement('.query'); // membuat test menjadi gagal
-    I.see('Tidak ada film untuk ditampilkan', '.restaurant-item__not__found');
-});
+Scenario('favoriting one restaurant', (I) => {
+  I.see('Tidak ada film untuk ditampilkan', '.restaurant-item__not__found');
 
-Scenario('favoriting one restaurant', ({ I }) => {
-    I.see('Tidak ada film untuk ditampilkan', '.restaurant-item__not__found');
+  I.amOnPage('/');
 
-    I.amOnPage('/');
-    pause();
+  I.seeElement('.restaurant__title a');
+  I.click(locate('.restaurant__title a').first());
+
+  I.seeElement('#favoriteButton');
+  I.click('#favoriteButton');
+
+  I.amOnPage('/#/favorite');
+  I.seeElement('.restaurant-item');
 });

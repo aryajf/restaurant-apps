6a9e007f01e45a6af9b8f6104a361f4b3694f5df
aryajf <aryajavasfatih888@gmail.com>
2022-03-10 11:50:29 +0700

    adding integration testing

--- src/scripts/views/templates/template-creator.js
+++ src/scripts/views/templates/template-creator.js
@@ -1,11 +1,11 @@
 const createFavoriteButtonTemplate = () => `
-  <button aria-label="favorite this movie" id="favoriteButton" class="favorite">
+  <button aria-label="favorite this restaurant" id="favoriteButton" class="favorite">
      <i class="fa fa-heart-o" aria-hidden="true"></i>
   </button>
 `;
 
 const createFavoritedButtonTemplate = () => `
-  <button aria-label="unfavorite this movie" id="favoriteButton" class="favorite">
+  <button aria-label="unfavorite this restaurant" id="favoriteButton" class="favorite">
     <i class="fa fa-heart" aria-hidden="true"></i>
   </button>
 `;
